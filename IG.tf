6a8bf2c0fb9a807308c42716509067e6d8e1ddc9
EC2 Default User <ec2-user@ip-172-31-19-135.eu-west-2.compute.internal>
2020-05-06 21:21:29 +0000

    added auto scaling group module

---
+++ IG.tf
@@ -0,0 +1,4 @@
+resource "aws_internet_gateway" "gw" {
+#   vpc_id = "${aws_vpc.default.id}"
+    vpc_id = "vpc-08646c74979e98354"
+}
---
+++ provider.tf
@@ -0,0 +1,18 @@
+provider "aws" { 
+region = "${var.region}" 
+# version = "2.59" 
+} 
+
+terraform { 
+required_version = "0.11.14" 
+backend "s3" { 
+bucket = "state-class-aidar" 
+key = "path/to/my/key" 
+region = "us-east-1" 
+} 
+} 
+
+
+
+
+
